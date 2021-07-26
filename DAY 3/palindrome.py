759d6ca419db75f8b92462eab46f122d99a98aa7
CHEVULAHAARVISH <76098768+CHEVULAHAARVISH@users.noreply.github.com>
2021-07-26 22:20:17 +0530

    Add files via upload

---
+++ DAY 3/palindrome.py
@@ -0,0 +1,13 @@
+#palindrome.py
+x=int(input('GIVE RANDOM NUMBER:'))
+
+temp=x
+rev=0
+while(x>0):
+    dig=x%10
+    rev=rev*10+dig
+    x=x//10
+if(temp==rev):
+    print("The number is a palindrome!")
+else:
+    print("The number isn't a palindrome!")
