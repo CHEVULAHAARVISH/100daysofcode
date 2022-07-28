83a91564152e26bf095d91419b3f224261446f43
CHEVULA HAARVISH <76098768+CHEVULAHAARVISH@users.noreply.github.com>
2022-07-28 20:59:01 +0530

    Add files via upload

---
+++ DAY 10/day 10.py
@@ -0,0 +1,21 @@
+
+
+#priyanka
+
+
+
+
+
+if __name__ == '__main__':
+    i=int(input("CHOOSE ONE NUMBER :"))
+    if i%2==1:
+        x=print('weird')
+    elif i>20 or 2<=i<=5:
+        x=print("NOT WEIRD")
+    elif 6<=i<=20:
+        x=print("Weird")
+
+
+
+
+
